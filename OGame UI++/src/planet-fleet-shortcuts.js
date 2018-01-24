b87f81e03692c72b2ee17e407fb1661ce334b389
tsaikd <tsaikd@gmail.com>
2018-01-24 15:22:12 +0800

    add expedition link in planet fleet shortcuts
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- OGame UI++/src/planet-fleet-shortcuts.js
+++ OGame UI++/src/planet-fleet-shortcuts.js
@@ -3,6 +3,7 @@ var fn = function () {
   window._addPlanetFleetShortcuts = function _addPlanetFleetShortcuts () {
     $('div#planetList > .smallplanet').each(function (_, planet) {
       var coordinates = getPlanetCoords($(planet));
+      var moonID = getMoonID(planet);
       var url = '/game/index.php?page=fleet1';
       url += '&galaxy=' + coordinates[0];
       url += '&system=' + coordinates[1];
@@ -11,6 +12,12 @@ var fn = function () {
 
       var transportLinkUrl = url + '&mission=3';
       var deploymentLinkUrl = url + '&mission=4';
+      var expeditionLinkUrl = '/game/index.php?page=fleet1';
+      expeditionLinkUrl += '&galaxy=' + coordinates[0];
+      expeditionLinkUrl += '&system=' + coordinates[1];
+      expeditionLinkUrl += '&position=16&type=1';
+      var expeditionMoonLinkUrl = expeditionLinkUrl + '&cp=' + moonID;
+      expeditionLinkUrl += '&cp=' + getPlanetID(planet);
 
       $(planet).css('position', 'relative');
 
@@ -28,8 +35,26 @@ var fn = function () {
         '<img src="' + window.uipp_images.stay + '" style="height:100%;width:100%;"/>',
         '</a>'
       ].join('');
+      var expeditionLink = [
+        '<a class="transDeployLink"',
+        ' style="position:absolute;left:3px;top:-8px;padding:0 2px;display:inline-block;z-index:999;background-color:rgba(255,255,255,0.3);"',
+        ' href="' + expeditionLinkUrl + '">',
+        'EX',
+        '</a>'
+      ].join('');
+      var expeditionMoonLink = [
+        '<a class="transDeployLink"',
+        ' style="position:absolute;left:24px;top:-8px;padding:0 2px;display:inline-block;z-index:999;background-color:rgba(255,255,255,0.3);"',
+        ' href="' + expeditionMoonLinkUrl + '">',
+        'EXM',
+        '</a>'
+      ].join('');
       $(planet).append(deploymentLink);
       $(planet).append(transportLink);
+      $(planet).append(expeditionLink);
+      if (moonID) {
+        $(planet).append(expeditionMoonLink);
+      }
     });
 
     function getPlanetCoords (planet) {
@@ -38,6 +63,21 @@ var fn = function () {
       var end = coordsAsText.indexOf(']');
       var trimmedCoords = coordsAsText.substr(start + 1, end - start - 1);
       return trimmedCoords.split(':');
+    }
+
+    function getPlanetID (planet) {
+      var attrID = $(planet).attr('id') || ''
+      return attrID.replace(/^planet\-/, '') || ''
+    }
+
+    // return empty string if moon not found
+    function getMoonID (planet) {
+      var href = $(planet).find('.moonlink').attr('href');
+      var match = /cp=(\d+)/.exec(href);
+      if (match) {
+        return match[1]
+      }
+      return ''
     }
   };
 };
